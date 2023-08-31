f57d90ff21f84bd8f07642ba4be1484ef8e766eb
ramonjurelle <70752313+ramonjurelle@users.noreply.github.com>
2023-08-31 20:57:48 +0800

    CS4TruthPerez.java

--- src/cs4truthperez/CS4TruthPerez.java
+++ src/cs4truthperez/CS4TruthPerez.java
@@ -18,18 +18,26 @@ public class CS4TruthPerez {
         // TODO code application logic here
         String name1 = "Mark Tarcelo";
         String subject1 = "Computer Science";
-        String year1 = "7";
+        int year1 = "7";
         System.out.println("Teacher 1" + "\n" + "Name: " + name1 + "\n" + "Subject: " + subject1 + "\n" +  "Years of Teaching: " + year1 + "\n");
         
         String name2 = "Joshua Tarcelo";
         String subject2 = "Social Science";
-        String year2 = "4";
+        int year2 = "4";
         System.out.println("Teacher 2" + "\n" + "Name: " + name2 + "\n" + "Subject: " + subject2 + "\n" +  "Years of Teaching: " + year2 + "\n");
         
         String name3 = "Ayaka Tarcelo";
         String subject3 = "Study Period";
-        String year3 = "0";
+        int year3 = "0";
         System.out.println("Teacher 3" + "\n" + "Name: " + name3 + "\n" + "Subject: " + subject3 + "\n" +  "Years of Teaching: " + year3 + "\n");
+    
+        int totalYears = year1 + year2 + year3;
+        System.out.println("Total Years of teaching: " + totalYears);
+
+        boolean year2longest = (year2 > year1) && (year2 > year3);
+        System.out.println("Joshua Tarcelo has the most years of teaching: " + year2longest);
+
+        boolean sameSub = (subject1 == subject2) && (subject1 == subject3) && (subject2 == subject3);
     }
     
 }
